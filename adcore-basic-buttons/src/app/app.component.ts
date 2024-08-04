6336ab31fdfde8c3365686f1c74011623ef427ad
romi kamin <romik@moveo.co.il>
2024-08-04 16:18:57 +0300

    I added the Angular Material buttons code.

--- adcore-basic-buttons/src/app/app.component.ts
+++ adcore-basic-buttons/src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BasicButtonsComponent } from './basic-buttons/basic-buttons.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
+/**
+ * @title Basic buttons
+ */
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    BasicButtonsComponent,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'adcore-basic-buttons';
---
+++ adcore-basic-buttons/src/app/basic-buttons/basic-buttons.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-basic-buttons',
+  standalone: true,
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  templateUrl: './basic-buttons.component.html',
+  styleUrl: './basic-buttons.component.css',
+})
+export class BasicButtonsComponent {}
