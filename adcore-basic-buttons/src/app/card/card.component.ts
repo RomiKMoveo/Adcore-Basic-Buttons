45b7d16ca1864f1ec3b66bde41f93ba2766830d0
romi kamin <romik@moveo.co.il>
2024-08-13 12:28:54 +0300

    imp card with custom style

---
+++ adcore-basic-buttons/src/app/card/card.component.ts
@@ -0,0 +1,31 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatChipsModule } from '@angular/material/chips';
+
+/**
+ * @title Basic cards
+ */
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatProgressBarModule,
+    MatChipsModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css',
+})
+export class CardComponent {
+  longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
+  Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
+  usually kept as a companion animal or for showing.`;
+}
