4bfced16e7b5a598efb6f52100768d0f3e7848ed
romi kamin <romik@moveo.co.il>
2024-08-20 19:04:41 +0300

    basic input pixel perfect

--- adcore-basic-buttons/src/app/list/list.component.ts
+++ adcore-basic-buttons/src/app/list/list.component.ts
@@ -1,12 +1,65 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+export interface Section {
+  name: string;
+  updated: Date;
+}
 
+/**
+ * @title Basic list
+ */
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatListModule,
+    MatIconModule,
+    MatDividerModule,
+    DatePipe,
+    HttpClientModule,
+  ],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
 })
 export class ListComponent {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'unicorn',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../assets/unicorn_icon.svg'
+      )
+    );
+  }
 
+  typesOfShoes: string[] = [
+    'Boots',
+    'Clogs',
+    'Loafers',
+    'Moccasins',
+    'Sneakers',
+  ];
+
+  folders: Section[] = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    },
+  ];
 }
