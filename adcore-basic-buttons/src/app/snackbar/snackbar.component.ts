103c5f91e2b770e15571538da595e36f4cdd6ac3
romi kamin <romik@moveo.co.il>
2024-08-28 15:56:44 +0300

    snackbar style

--- adcore-basic-buttons/src/app/snackbar/snackbar.component.ts
+++ adcore-basic-buttons/src/app/snackbar/snackbar.component.ts
@@ -1,8 +1,15 @@
 import { Component, inject } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  MatSnackBar,
+  MatSnackBarAction,
+  MatSnackBarActions,
+  MatSnackBarLabel,
+  MatSnackBarRef,
+} from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-snackbar',
@@ -13,10 +20,41 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 })
 export class SnackbarComponent {
   private _snackBar = inject(MatSnackBar);
-  massage = '✅  Proposal shared successfully!';
-  action = 'x';
+  // massage = '✅  Proposal shared successfully!';
+  // action = 'x';
 
+  // openSnackBar() {
+  //   this._snackBar.open(this.massage, this.action);
+  // }
+  durationInSeconds = 5000;
   openSnackBar() {
-    this._snackBar.open(this.massage, this.action);
+    this._snackBar.openFromComponent(PizzaPartyAnnotatedComponent, {
+      duration: this.durationInSeconds * 1000,
+    });
   }
+}
+
+@Component({
+  selector: 'snack-bar-annotated-component-example-snack',
+  templateUrl: 'snackbar.html',
+  styles: `
+    :host {
+      display: flex;
+    }
+
+    .example-pizza-party {
+      color: hotpink;
+    }
+  `,
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatSnackBarLabel,
+    MatSnackBarActions,
+    MatSnackBarAction,
+    MatIcon,
+  ],
+})
+export class PizzaPartyAnnotatedComponent {
+  snackBarRef = inject(MatSnackBarRef);
 }
