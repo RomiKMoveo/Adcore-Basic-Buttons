4764dc6ebc927df2e9ff62351d1fdd748ae991f6
romi kamin <romik@moveo.co.il>
2024-09-08 17:39:35 +0300

    cdkMenu with style (not all done )

--- adcore-basic-buttons/src/app/cdk-menu/cdk-menu.component.ts
+++ adcore-basic-buttons/src/app/cdk-menu/cdk-menu.component.ts
@@ -1,11 +1,44 @@
+import {
+  CdkContextMenuTrigger,
+  CdkMenu,
+  CdkMenuBar,
+  CdkMenuGroup,
+  CdkMenuItem,
+  CdkMenuItemCheckbox,
+  CdkMenuItemRadio,
+  CdkMenuTrigger,
+} from '@angular/cdk/menu';
 import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-cdk-menu',
   standalone: true,
-  imports: [MatDividerModule],
+  imports: [
+    MatDividerModule,
+    CdkContextMenuTrigger,
+    CdkMenu,
+    CdkMenuItem,
+    CdkMenuBar,
+    CdkMenuItem,
+    CdkMenuTrigger,
+    CdkMenuGroup,
+    CdkMenuItemCheckbox,
+    CdkMenuItemRadio,
+  ],
   templateUrl: './cdk-menu.component.html',
   styleUrl: './cdk-menu.component.css',
 })
-export class CdkMenuComponent {}
+export class CdkMenuComponent {
+  bold = false;
+  italic = false;
+
+  sizes = ['Small', 'Normal', 'Large'];
+  selectedSize: string | undefined = 'Normal';
+
+  reset() {
+    this.bold = false;
+    this.italic = false;
+    this.selectedSize = 'Normal';
+  }
+}
