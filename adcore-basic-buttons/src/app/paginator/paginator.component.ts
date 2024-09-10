ed6093a65977127897ae026b10efed7af2c8df2e
romi kamin <romik@moveo.co.il>
2024-09-10 11:43:44 +0300

    paginator with style

--- adcore-basic-buttons/src/app/paginator/paginator.component.ts
+++ adcore-basic-buttons/src/app/paginator/paginator.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
+import { Component } from '@angular/core';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-paginator',
   standalone: true,
-  imports: [MatDividerModule],
+  imports: [
+    MatDividerModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatSlideToggleModule,
+    JsonPipe,
+  ],
   templateUrl: './paginator.component.html',
   styleUrl: './paginator.component.css',
 })
-export class PaginatorComponent {}
+export class PaginatorComponent {
+  length = 50;
+  pageSize = 10;
+  pageIndex = 0;
+  pageSizeOptions = [5, 10, 25];
+
+  hidePageSize = false;
+  showPageSizeOptions = true;
+  showFirstLastButtons = true;
+  disabled = false;
+
+  pageEvent!: PageEvent;
+
+  handlePageEvent(e: PageEvent) {
+    this.pageEvent = e;
+    this.length = e.length;
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+  }
+
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput
+        .split(',')
+        .map((str) => +str);
+    }
+  }
+}
